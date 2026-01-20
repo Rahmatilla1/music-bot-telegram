d4ae2de4ffc57561ef9606d5aefa1ba4b17cd4b4
Rahmatilla1 <xikmatillr@gmail.com>
2026-01-20 11:07:14 +0500

    Initial commit

--- server.py
+++ server.py
@@ -513,7 +513,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Bot is running")
+        self.wfile.write("Bot is running")
 
 def run_server():
     port = int(os.environ.get("PORT", 10000))
